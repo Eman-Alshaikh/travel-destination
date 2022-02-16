c07f74bf93b26fcd3e15464c7bc39f2810ca3a45
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-02-16 16:07:18 +0200

    task 16 initial at 4 pm

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,9 @@
-import '/home/student/travel-destination/src/components/footer/Footer.css';
-
+import '../footer/Footer.css';
+ 
 function Footer() {
     return (
         <>
-            <div className="footermm">
+            <div className="myfooter">
                 <h3 >Author :Eman Al-shaikh  </h3>
                      <a href="https://www.linkedin.com/in/eman-al-shaikh-9a4583194/" target="_blank" rel="noreferrer"> LinkedIn </a>
              </div>
